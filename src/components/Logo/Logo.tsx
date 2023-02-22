f27bb5cd494fd46581d24b59ada24491247be6b8
Timofii <unicondor.tv@gmail.com>
2023-02-22 18:10:54 +0200

    add logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,12 @@
 import Typography from '@mui/material/Typography'
-import logo from 'assets/logo-Mountain.svg'
+import logo from 'assets/Logo-connect.svg'
 
 type Props = {}
 
 const Logo = (props: Props) => {
     return (
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <img src={logo} alt="mountain-project" />
+            <img src={logo} alt="connect-project" />
         </Typography>
     )
 }
