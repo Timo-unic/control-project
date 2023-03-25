d96be15bb7687f1a7c14494a6158cc89aa0404a9
Timofii <unicondor.tv@gmail.com>
2023-03-26 00:53:56 +0200

    add and styled page Join Us

--- src/pages/JoinUs/JoinUs.tsx
+++ src/pages/JoinUs/JoinUs.tsx
@@ -1,6 +1,9 @@
-import { Container, Typography } from '@mui/material'
+import { Container } from '@mui/material'
 import './JoinUs.scss'
 import joinusgrad from 'assets/join-gradient.png'
+import iconusefulinfo from 'assets/contact-icon.png'
+import PhoneIcon from '@mui/icons-material/Phone'
+import EmailIcon from '@mui/icons-material/Email'
 
 type Props = {}
 
@@ -46,8 +49,60 @@ const JoinUs = (props: Props) => {
                         </div>
                     </div>
                     <div className="joinus-main-useful">
-                        <div className="joinus-main-info">Useful info</div>
-                        <div className="joinus-main-contacts">Contact us</div>
+                        <div className="joinus-main-info">
+                            <div className="joinus-main-info-title">
+                                Useful info
+                            </div>
+                            <div className="joinus-main-info-item">
+                                <div>
+                                    <img src={iconusefulinfo} alt="" />
+                                </div>
+                                <span className="joinus-main-useful-text">
+                                    Poster pentolum quantum pored dolor sit
+                                    aisicing elit
+                                </span>
+                            </div>
+                            <div className="joinus-main-info-item">
+                                <div>
+                                    <img src={iconusefulinfo} alt="" />
+                                </div>
+                                <span className="joinus-main-useful-text">
+                                    Lorem ipsum dolor sit amet tetur adipisicing
+                                </span>
+                            </div>
+                            <div className="joinus-main-info-item">
+                                <div>
+                                    <img src={iconusefulinfo} alt="" />
+                                </div>
+                                <span className="joinus-main-useful-text">
+                                    Estrop pner valanti osteromus elit via
+                                    modore erondero patern
+                                </span>
+                            </div>
+                        </div>
+                        <div className="joinus-main-contacts">
+                            <div className="joinus-main-contacts-title">
+                                Contact us
+                            </div>
+                            <div className="joinus-main-contacts-text">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labor
+                            </div>
+                            <div className="joinus-main-contacts-icon">
+                                <PhoneIcon sx={{ margin: '0 10px' }} />
+                                <span className="joinus-main-useful-text">
+                                    +01 203 4040 3333
+                                </span>
+                            </div>
+
+                            <div className="joinus-main-contacts-icon">
+                                <EmailIcon sx={{ margin: '0 10px' }} />
+                                <span className="joinus-main-useful-text">
+                                    noreply@envato.com
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </Container>
             </Container>
