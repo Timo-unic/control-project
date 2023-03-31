70c60bcdc50268f20d088b95fbf4d8b42e0af04a
Timofii <unicondor.tv@gmail.com>
2023-03-31 17:27:27 +0300

    audit all files

--- src/components/FavoritComponents/FavoritArticlesList.tsx
+++ src/components/FavoritComponents/FavoritArticlesList.tsx
@@ -29,83 +29,82 @@ const FavoritArticlesList = ({
     date,
     image,
 }: Props) => {
-
     const scrollUp = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth',
         })
     }
-    
+
     const isLiked = useAppSelector((state) => state.articlesLikeState[id])
 
     const dispatch = useAppDispatch()
 
-  return (
-    <Card variant="outlined" className="places-article-content">
-    <CardContent>
-        <div className="places-image">
-            <Button
-                variant="outlined"
-                className="article-image-likebtn"
-                onClick={() =>
-                    isLiked
-                        ? dispatch(removeLike(id))
-                        : dispatch(addLike(id))
-                }
-            >
-                {isLiked ? (
-                    <FavoriteIcon sx={{ color: 'white' }} />
-                ) : (
-                    <FavoriteBorderIcon sx={{ color: 'white' }} />
-                )}
-            </Button>
-            <img src={image} alt="" className="places-image-style" />
-        </div>
-    </CardContent>
-    <CardContent className="places-article-block">
-        <CardActions>
-            <Button
-                variant="outlined"
-                className="places-btn"
-                onClick={scrollUp}
-            >
-                <Link to={linkpage} className="places-btn-link">
-                    {category}
-                </Link>
-            </Button>
-        </CardActions>
-        <div className="places-title">
-            <Link
-                to={`/articles/${id}`}
-                className="places-title-link"
-                onClick={scrollUp}
-            >
-                {title}
-            </Link>
-        </div>
-        <div className="places-paragraph">{paragraph}</div>
-        <div className="places-footer">
-            <div className="places-footer-blogger">
-                <AccountCircleIcon color="action" />
-                <span className="places-footer-style">
-                    <Link
-                        to={`/author/muffin`}
-                        className="group-muffin-link"
+    return (
+        <Card variant="outlined" className="places-article-content">
+            <CardContent>
+                <div className="places-image">
+                    <Button
+                        variant="outlined"
+                        className="article-image-likebtn"
+                        onClick={() =>
+                            isLiked
+                                ? dispatch(removeLike(id))
+                                : dispatch(addLike(id))
+                        }
+                    >
+                        {isLiked ? (
+                            <FavoriteIcon sx={{ color: 'white' }} />
+                        ) : (
+                            <FavoriteBorderIcon sx={{ color: 'white' }} />
+                        )}
+                    </Button>
+                    <img src={image} alt="" className="places-image-style" />
+                </div>
+            </CardContent>
+            <CardContent className="places-article-block">
+                <CardActions>
+                    <Button
+                        variant="outlined"
+                        className="places-btn"
                         onClick={scrollUp}
                     >
-                        {author}
+                        <Link to={linkpage} className="places-btn-link">
+                            {category}
+                        </Link>
+                    </Button>
+                </CardActions>
+                <div className="places-title">
+                    <Link
+                        to={`/articles/${id}`}
+                        className="places-title-link"
+                        onClick={scrollUp}
+                    >
+                        {title}
                     </Link>
-                </span>
-            </div>
-            <div className="places-footer-date">
-                <AccessTimeIcon />
-                <span className="places-footer-style">{date}</span>
-            </div>
-        </div>
-    </CardContent>
-</Card>
-  )
+                </div>
+                <div className="places-paragraph">{paragraph}</div>
+                <div className="places-footer">
+                    <div className="places-footer-blogger">
+                        <AccountCircleIcon color="action" />
+                        <span className="places-footer-style">
+                            <Link
+                                to={`/author/muffin`}
+                                className="group-muffin-link"
+                                onClick={scrollUp}
+                            >
+                                {author}
+                            </Link>
+                        </span>
+                    </div>
+                    <div className="places-footer-date">
+                        <AccessTimeIcon />
+                        <span className="places-footer-style">{date}</span>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    )
 }
 
 export default FavoritArticlesList
